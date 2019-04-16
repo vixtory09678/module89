d200cce028b748c5c23d5e120e9cfdf495130bd9
roboticsza <robotics.za@gmail.com>
2019-04-16 20:37:37 +0700

    add version seccess for communication

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,30 @@
 #include <mbed.h>
 #include "joint_config.h"
 #include "protocol_config.h"
+#include "driveMotor.h"
 #include <AS5600.h>
-#include <math.h>
 
-AS5600 encoder(B9,B8);//SDA SCL
-Timer t;
+AS5600 encoder(SDA_PIN,SCL_PIN);//SDA SCL
+Timer timeSpan;
+
+// ------------------- CONFIG --------------------------//
+// #define DEBUG
+#ifdef DEBUG
+Serial pc(A9,A10);
+#endif
+// ----------------- END CONFIG ------------------------//
 
 int  getAngle(){
   return ((encoder.getAngleAbsolute() + (2047 - SET_ZERO)) % 4096) - 2047;
 }
 
-int toEncoder(float degree){
-  return (config._coeff / 360.0) * 4095;
+float toEncoder(float degree){
+  return (degree / 360.0) * 4095.0;
 }
 
 int indexCalculate = 0;
 
-#define T_PERIOD 0.002f
+#define T_PERIOD 0.01
 inline void compute(){
   switch (instruct)
   {
@@ -44,7 +51,6 @@ inline void compute(){
                     (3.0 * slave_joint1[indexCalculate + 3]._coeff * tp2);        // 3 * c3 * t^2
 
     if (trajectFb.time >= slave_joint1[indexCalculate + 4]._coeff){
-      
       if (indexCalculate < 6)
         indexCalculate += 1;
       else
@@ -53,6 +59,21 @@ inline void compute(){
       configFb.tua = configFb.Kp * (configFb.error)
                     + configFb.Kd * ((q_dot_ref - configFb.error - configFb.lastError) / T_PERIOD);
     }
+    break;
+  }
+}
+
+inline void state(){
+  switch (instruct)
+  {
+  case CONFIGURATION:
+    // driveMotor()
+    break;
+  case TRAJECTORY:
+
+    break;
+  case WAITING:
+
     break;
   }
 }
@@ -69,6 +90,11 @@ inline void onDataReceive(){
         configFb.error = 0.0;
         configFb.lastError = 0.0;
         configFb.tua = 0.0;
+
+        #ifdef DEBUG
+        pc.printf("con %f\n",slave_joint1[0]._coeff);
+        #endif
+
         break;
       case TRAJECTORY:
         /* code */
@@ -78,18 +104,26 @@ inline void onDataReceive(){
         trajectFb.error = 0.0;
         trajectFb.lastError = 0.0;
         trajectFb.tua = 0.0;
+
+        #ifdef DEBUG
+        pc.printf("tra %f\n",slave_joint1[3]._coeff);
+        #endif
+
         break;
 
       default:
         break;
     }
     isReadDyProtocol = false;
+    timeSpan.start();
   }
 }
 
 
 inline void setup(){
-  // computeFeedback.attach(compute,T_PERIOD);
+
+  // setupMotor();
+  led = 0;
   device.format(8,1);
   encoder.init();
 }
@@ -100,22 +134,6 @@ int main() {
     // put your main code here, to run repeatedly:
     checkReceiveData();
     onDataReceive();
-
-    // if (device.receive()){
-    //   // out = 1;
-    //   device.reply(device.read());
-    //   // out = 0;
-    // }
-    // if(device.receive()) {
-    //   for(int i = 0; i < 4 ; i++){
-    //     data[i] = device.read();
-    //   }
-    //   device.reply(111);
-
-    //   pc.printf("%d\n",sizeof(data)/sizeof(data[0]));
-    // }
-    // printf("data is %d\n",toEncoder(getAngle()));
-    // wait(0.1);
-    
+    state();
   }
 }

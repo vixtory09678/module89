c62e80ec896c9623c02edebc96ec299c08cda5e7
roboticsza <robotics.za@gmail.com>
2019-05-01 04:18:50 +0700

    motor & joint config update

--- include/driveMotor.h
+++ include/driveMotor.h
@@ -1,6 +1,7 @@
 #ifndef DRIVE_MOTOR_H
 #define DRIVE_MOTOR_H
 #include <mbed.h>
+// #include "joint_config.h"
 
 #define TO_PERIOD(hz) (float)(1.0/hz)
 #define CONSTAIN(x,min,max) (x > max) ? max : (x < min) ? min : x
@@ -10,12 +11,15 @@ PwmOut tuaPin(B3);
 
 inline void driveMotor(int direction, float tua){
     dirPin.write(direction);
-    tua = CONSTAIN(tua, 0, 100);
+    if(tua != 0)
+        tua = CONSTAIN(tua, 5, 60);
+    else
+        tua = 0.0;
     tuaPin.write(tua/100.0);
 }
 inline void setupMotor(){
     dirPin = 0;
-    tuaPin.period(TO_PERIOD(1000));
+    tuaPin.period(TO_PERIOD(500));
     tuaPin.write(0.0);
 }
 
--- include/joint_config.h
+++ include/joint_config.h
@@ -24,10 +24,49 @@ typedef struct _feedback{
   float time;
 }FeedbackControl;
 
+#if (JOINT_NUM == 1)
+    #define JOINT_LIMIT 150
+    #define SET_ZERO 1576
+    #define SDA_PIN B9
+    #define SCL_PIN B8
+    #define INVERSE -1
+    #define DIR_JOINT 1
+    #define LIMIT_MIN_TUA 2000.0f
+    #define LIMIT_MAX_TUA 10000.0f
+    #define KP 1.0
+    #define KI 0.0
+    #define KD 0.0
+#elif (JOINT_NUM == 2)
+    #define JOINT_LIMIT 90
+    #define SET_ZERO 1648
+    #define SDA_PIN B9
+    #define SCL_PIN B8
+    #define INVERSE 1
+    #define DIR_JOINT 0
+    #define LIMIT_MIN_TUA 10.0f
+    #define LIMIT_MAX_TUA 500.0f
+    #define KP 1.0
+    #define KI 0.0
+    #define KD 0.0
+#elif (JOINT_NUM == 3)
+    #define JOINT_LIMIT 90
+    #define SET_ZERO 1299
+    #define SDA_PIN B9
+    #define SCL_PIN B8
+    #define INVERSE -1
+    #define DIR_JOINT 0
+    #define LIMIT_MIN_TUA 2000.0f
+    #define LIMIT_MAX_TUA 10000.0f
+    #define KP 6.0
+    #define KI 0.0
+    #define KD 1.0
+#endif
+
 FeedbackControl configFb = {
-  0.0f,     // Kp
-  0.0f,     // Ki
-  0.0f,     // Kd
+  KP,     // Kp
+  KI,     // Ki
+  KD,     // Kd
+  
   0.0f,     // error
   0.0f,     // lastError
   0.0f,     // tua
@@ -35,32 +74,14 @@ FeedbackControl configFb = {
 };
 
 FeedbackControl trajectFb = {
-  0.0f,     // Kp
-  0.0f,     // Ki
-  0.0f,     // Kd
+  KP,     // Kp
+  KI,     // Ki
+  KD,     // Kd
+
   0.0f,     // error
   0.0f,     // lastError
   0.0f,     // tua
   0.0f      // time
 };
-
-
-#if (JOINT_NUM == 1)
-    #define JOINT_LIMIT 150
-    #define SET_ZERO 523
-    #define SDA_PIN B9
-    #define SCL_PIN B8
-#elif (JOINT_NUM == 2)
-    #define JOINT_LIMIT 90
-    #define SET_ZERO 1642
-    #define SDA_PIN B9
-    #define SCL_PIN B8
-#elif (JOINT_NUM == 3)
-    #define JOINT_LIMIT 90
-    #define SET_ZERO 2678
-    #define SDA_PIN B9
-    #define SCL_PIN B8
-#endif
-
 
 #endif
